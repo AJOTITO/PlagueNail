45ddeb1d85f852a7c3191126256e41add2a0012b
jesusperez00 <jesusperez00sonido@gmail.com>
2024-08-11 14:50:47 +0200

    mosquitos spawn y movimiento

--- Assets/Scripts/MosquitoMovement.cs
+++ Assets/Scripts/MosquitoMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Prueba : MonoBehaviour
+public class MosquitoMovement : MonoBehaviour
 {
+
+    public Transform playerTransform;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = playerTransform.position;
     }
 }
